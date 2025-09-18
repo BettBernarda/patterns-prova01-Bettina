0d49d086bde39c2e91d88ba39cd33820d98ab977
Bettina Bernarda da Silva <megan.silva182006@gmail.com>
2025-09-18 20:14:15 -0300

    Update index01.js

--- desafio01/index01.js
+++ desafio01/index01.js
@@ -1,1 +1,71 @@
-console.log("Hello, World!");
+class Lanche {
+  constructor(pao, carne, queijo, salada, molho) {
+    this.pao = pao;
+    this.carne = carne;
+    this.queijo = queijo;
+    this.salada = salada;
+    this.molho = molho;
+  }
+
+  show() {
+    console.log("Lanche:", {
+      pao: this.pao,
+      carne: this.carne,
+      queijo: this.queijo,
+      salada: this.salada,
+      molho: this.molho,
+    });
+  }
+
+}
+
+class LancheBuilder {
+  constructor(){
+    this.lanche =new Lanche();
+  }
+  setPao() {
+    this.lanche.pao = true;
+    return this;
+  }
+  setCarne(){
+    this.lanche.carne = true;
+    return this;
+  }
+   setQueijo(){
+    this.lanche.queijo = true;
+    return this;
+  }
+   setSalada(){
+    this.lanche.salada = true;
+    return this;
+  }
+   setMolho(){
+    this.lanche.molho = true;
+    return this;
+  }
+  build() {
+    return this.lanche;
+  }
+}
+
+class LancheDirector{
+  static buildLanchepadrao(){
+    return new LancheBuilder()
+    .setCarne()
+    .setMolho()
+    .setQueijo()
+    .build()
+  }
+  static buildPaoComBerro() {
+    return new LancheBuilder()
+    .setPao()
+    .build()
+    }
+  }
+
+
+const lancheSimples = LancheDirector.buildPaoComBerro()
+const lancheCompleto = LancheDirector.buildLanchepadrao()
+
+lancheSimples.show();
+lancheCompleto.show();
