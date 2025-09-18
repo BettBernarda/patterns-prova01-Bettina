8d8f0ca039b62a885b794a7c4aa65f3f423d5ef3
Bettina Bernarda da Silva <megan.silva182006@gmail.com>
2025-09-18 20:13:57 -0300

    Create index02.js

---
+++ desafio01/index02.js
@@ -0,0 +1,30 @@
+class Config {
+  constructor() {
+    if(Config.instance){
+      return Config.instance;
+    }
+    this.settings = [];
+    Config.instance = this;
+  }
+
+  setConfig(key, value) {
+    this.settings.push(key)
+    this.settings[key] = value;
+  }
+
+  getConfig(key) {
+    return this.settings[key];
+  }
+
+  printConfig(){
+    console.log("Configurações: /n", this.settings);
+  }
+}
+
+const c1 = new Config();
+c1.setConfig("lang", "pt-BR");
+
+const c2 = new Config();
+console.log(c2.getConfig("lang"));
+
+c1.printConfig()
